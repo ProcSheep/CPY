272b4c4dafe6ee71d56bd35c84865a488b73dda6
ProcSheep <3132519428@qq.com>
2025-10-20 23:39:54 +0800

    FIN:图片路由

--- nodeapp/routes/image.router.js
+++ nodeapp/routes/image.router.js
@@ -3,5 +3,6 @@ const imgRouter = express.Router()
 const imgController = require('../controllers/image.controller');
 
 imgRouter.get('/imgDownload', imgController.imgDownload)
+imgRouter.post('/upload',)
 
 module.exports = imgRouter
